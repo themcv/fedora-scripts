f0fb36209d4892391104ee8a74822b75c3042f2a
themcv <jesse.roe14@gmail.com>
2025-07-03 21:31:07 -0400

    Update fedora-base-nvidia.sh
    Add remainder of install stuff

--- fedora-base-nvidia.sh
+++ fedora-base-nvidia.sh
@@ -7,7 +7,18 @@
 
 fv=$(cat /etc/os-release | grep VERSION_ID | cut -d = -f2)
 UPDATES=$(dnf check-update --quiet | grep -Ev 'Last metadata expiration|^$' | wc -l)
+sb=$(mokutil --sb-state | grep disabled | wc -l)
+nv=$(modinfo -F version nvidia)
 
+# Check if SecureBoot is enabled
+if [ "$sb" -lt 1 ]; then
+    echo "SecureBoot is currently enabled"
+    echo "This script does not support having SecureBoot enabled at this time"
+    echo "Please disable SecureBoot before running it again"
+    break
+else
+    echo "SecureBoot is disabled."
+    
 # Check and make sure that system is up to date
 # Update the system
 echo "Checking for updates before continuing"
@@ -29,12 +40,14 @@ else
 fi
 
 
-# Install the RPM fusion repos and epel
+# Install the RPM fusion repos
 echo "Installing RPM fusion free repos for Fedora $fv"
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$fv.noarch.rpm
 
 echo "Installing RPM fusion non-free repos for Fedora $fv
 dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$fv.noarch.rpm
+
+# Start the QOL stuff
 
 echo "Swapping ffmpeg"
 dnf swap ffmpeg-free ffmpeg --allowerasing
@@ -42,6 +55,21 @@ dnf swap ffmpeg-free ffmpeg --allowerasing
 echo "Installing Multimedia codecs"
 dnf install @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 
+echo "Installing VAAPI"
+dnf install libva-nvidia-driver
+
+echo "Installing NVIDIA drivers"
+dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
+
+echo "Waiting on akmods to finsh - this will take a bit
+while $nv | grep 'ERROR';
+    do
+      for i in "${spin[@]}"
+      do
+        echo -ne "\b$i"
+        sleep 0.1
+  done
+done
 
 
 
